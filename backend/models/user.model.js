576cb9dde2b18efd8d5167d28d90303c4bd74a9e
mrizkyrn <mrizkyr2411@gmail.com>
2023-11-10 14:53:31 +0700

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+   {
+      username: {
+         type: String,
+         required: true,
+         unique: true,
+         minlength: 3,
+      },
+      password: {
+         type: String,
+         required: true,
+         minlength: 3,
+      },
+   },
+   { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

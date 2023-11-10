b70ff02b70e9882596079bd821380baf0553432f
mrizkyrn <mrizkyr2411@gmail.com>
2023-11-10 14:50:23 +0700

    connect to db

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+
+mongoose
+   .connect(process.env.MONGO_URL)
+   .then(() => {
+      console.log('Connected to MongoDB');
+   })
+   .catch((error) => {
+      console.log(error);
+   });
 
 const app = express();
 

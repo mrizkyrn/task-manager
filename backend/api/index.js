78a595646e2103501ee9b880336353acd053498d
mrizkyrn <mrizkyr2411@gmail.com>
2024-04-21 17:05:42 +0700

    change cors origin

--- backend/api/index.js
+++ backend/api/index.js
@@ -23,7 +23,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://task-master-manager.vercel.app', credentials: true }));
 app.use(cookieParser());
 
 app.listen(3000, () => {
